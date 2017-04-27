9f4feb36f9c88d88651f4ef2740e651f310923b4
Nikolaj Andersson <nikolajandersson@gmail.com>
2017-04-27 17:21:38 +0200

    tested accelerometer

---
+++ AccelerometerTest/AccelerometerTest.ino
@@ -0,0 +1,50 @@
+//connect 3.3v to AREF
+
+
+const int analogPinX = A0; 
+const int analogPinY = A2;
+const int analogPinZ = A4;  
+
+void setup() {
+  // initialize serial communications at 9600 bps:
+  Serial.begin(9600);
+ 
+}
+
+void loop() {
+  //analogReference(EXTERNAL);    //connect 3.3v to AREF
+  // read the analog in value:
+  int x = analogRead(analogPinX);            
+  // map it to the range of the analog out:
+  int velocityX = map(x, 0, 1023, 0, 127);
+  // change the analog out value:
+  delayMicroseconds(30); // from kyub                     
+  //
+  int y = analogRead(analogPinY);            
+  int velocityY = map(y, 0, 1023, 0, 127);
+
+  delayMicroseconds(30); // from kyub                     
+  //
+  int z = analogRead(analogPinZ);            
+  int velocityZ = map(z, 0, 1023, 0, 127);
+
+  // print the results to the serial monitor:
+  Serial.print("x = " );                       
+  Serial.print(x);      
+  Serial.print("\t mapped x = " );                       
+  Serial.print(velocityX); 
+  
+  Serial.print("\ty = " );                       
+  Serial.print(y);      
+  Serial.print("\t mapped y = " );                       
+  Serial.print(velocityY); 
+  
+  Serial.print("\tz = " );                       
+  Serial.print(z);      
+  Serial.print("\t mapped z = " );                       
+  Serial.println(velocityZ); 
+  
+  delay(200);                     
+}
+
+
